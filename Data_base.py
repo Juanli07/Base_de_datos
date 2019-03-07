3352f906f8134e9db425055e2901d0cefae64615
Juan López Ix <juan07alberto@gmail.com>
2019-03-07 16:05:28 -0600

    hhh

--- Data_base.py
+++ Data_base.py
@@ -1,34 +1,14 @@
-from Tkinter import *
+import pymysql
 
-def PrincipalWindow():
-    window = Tk()
-    search = StringVar()
-    window.geometry('900x700')
-    window.configure(bg = 'white')
-    window.title('Data base')
-    Button(window, text = 'Alta', command = quit).place(x= 100, y =600)
-    Button(window, text = 'Baja', command = quit).place(x= 150, y =600)
-    Button(window, text = 'Administrar', command = quit).place(x= 200, y =600)
-    Entry(window, justify = CENTER, width = 63, textvariable = search).place(x = 200, y = 85)
-    Button(window, text = 'Buscar', command = quit).place(x = 720, y = 80)
-    window.mainloop()
+def openDB(user, password, database):
+    #abre conexión con la base de datos.
+    db = pymysql.connect("localhost", user, password, database)
+    #prepaarar el cursor
+    global cursor = db.cursor()
 
-def Login():
-    window = Tk()
-    window.geometry('200x150')
-    window.configure(bg = 'beige')
-    window.title('Iniciar Sesion')
-    Label(window, text = 'Usuario :').pack()
-    name = StringVar()
-    Entry(window, justify = CENTER, textvariable = name).pack()
-    Label(window, text = 'Contrasena :').place(x=60, y=50)
-    password = StringVar()
-    Entry(window, show = '*', justify = CENTER, textvariable = password).place(x = 17, y = 70)
-    btn = Button(window, text = 'Entrar', command = PrincipalWindow).place(x = 65, y = 100)
-    window.mainloop()
-
-Login()
-
-#Campo de texto
-#n1 = StringVar()
-#Entry(window, justify = CENTER, textvariable=n1).pack()
+def addTable():
+    tablename = input("Indique el nombre de la tabla : ")
+    while(op != )
+    text = "create table "+tablename+" ("
+    while()
+    cursor.execute()
